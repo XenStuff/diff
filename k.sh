52a46b315a2a8c94751d870240a1301a6709ad33
xenxynon <flynryder427@gmail.com>
2026-02-19 01:10:10 +0530

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,1 +1,1 @@
-reflector --country India,Singapore,Japan --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+reflector --country India,Singapore,Japan --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
