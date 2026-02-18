f6614d6eefe2a503dac7bb1cbdf119264630bc67
xenxynon <flynryder427@gmail.com>
2026-02-18 20:24:05 +0530

    Update ke.sh

--- ke.sh
+++ ke.sh
@@ -1,1 +1,1 @@
-curl -C - -LO --resolve oxygenos.oneplus.net:443:23.48.224.239 https://oxygenos.oneplus.net/OnePlus7Oxygen_14.P.41_OTA_0410_all_2112101753_downgrade_da90e698650240ff.zip
+https://idx.crudeoil.workers.dev/0:/Pathologic%202%20%5BFitGirl%20Repack%5D.zip
